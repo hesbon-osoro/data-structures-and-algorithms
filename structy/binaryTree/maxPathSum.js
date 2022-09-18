02520c9b764cb182b3bc9dcfca3c6202975b9cae
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-19 00:03:15 +0300

    maxPathSum.js

---
+++ structy/binaryTree/maxPathSum.js
@@ -0,0 +1,33 @@
+// Binary Tree
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
+
+const maxPathSum = root => {
+  if (root === null) return -Infinity;
+  if (root.left === null && root.right === null) return root.val;
+  return root.val + Math.max(maxPathSum(root.left), maxPathSum(root.right));
+};
+console.log(maxPathSum(a));
