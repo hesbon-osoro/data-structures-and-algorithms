93a5c4cf1a53e5dbd11498e986c488e54114534e
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-28 11:49:29 +0300

    myPow.js

---
+++ leetcode/myPow.js
@@ -0,0 +1,12 @@
+/*
+Pow(x, n)(Medium)
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+*/
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+const myPow = function (x, n) {
+  return Math.pow(x, n); // x ** n
+};
