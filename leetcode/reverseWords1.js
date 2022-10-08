a82c129d2ef9d0e859a1abdb3b46a79551ac13e4
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-08 18:55:36 +0300

    one liner upgrade

--- leetcode/reverseWords1.js
+++ leetcode/reverseWords1.js
@@ -1,16 +1,21 @@
-function reverseWords(s) {
-  let res = [],
-    start = 0;
-  // every word will put into an array
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] !== ' ') {
-      start = i;
-      while (s[i] !== ' ' && i < s.length) {
-        i++;
-      }
-      res.push(s.slice(start, i));
-    }
-  }
-  return res.reverse().join(' ');
-}
+// function reverseWords(s) {
+//   let res = [],
+//     start = 0;
+//   // every word will put into an array
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] !== ' ') {
+//       start = i;
+//       while (s[i] !== ' ' && i < s.length) {
+//         i++;
+//       }
+//       res.push(s.slice(start, i));
+//     }
+//   }
+//   return res.reverse().join(' ');
+// }
 console.log(reverseWords('the sky is blue'));
+
+// One - Liner
+function reverseWords(s) {
+  return s.split(' ').filter(Boolean).reverse().join(' ');
+}
