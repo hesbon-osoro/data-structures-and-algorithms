d42d8ae4933d9e4a316903c1458c13776a2577df
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-21 11:20:42 +0300

    containsNearbyDuplicate.js

---
+++ leetcode/containsNearbyDuplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+const containsNearbyDuplicate = function (nums, k) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i]) && i - map.get(nums[i]) <= k) {
+      return true;
+    }
+    map.set(nums[i], i);
+  }
+  return false;
+};
