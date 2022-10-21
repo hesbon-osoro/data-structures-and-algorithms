3b7e32c724cb0cd77b07e38fc3a086dccfee8a9a
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-21 22:46:53 +0300

    preorderTraversal.js

---
+++ leetcode/preorderTraversal.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const preorderTraversal = function (root) {
+  return root
+    ? [
+        root.val,
+        ...preorderTraversal(root.left),
+        ...preorderTraversal(root.right),
+      ]
+    : [];
+};
