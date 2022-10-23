8e66980f6457c3f42da43bb08ae80824078fcfd2
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-23 23:12:08 +0300

    change.js

---
+++ leetcode/change.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+const change = function (amount, coins) {
+  const dp = new Array(amount + 1).fill(0);
+  dp[0] = 1;
+  for (let coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] += dp[i - coin];
+    }
+  }
+  return dp[amount];
+};
