a85a03559c1589f1d778752ba197c2761d2f9208
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-30 13:44:17 +0300

    sortList.js

---
+++ leetcode/sortList.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const sortList = function (head) {
+  if (!head || !head.next) return head;
+  let slow = head;
+  let fast = head;
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  const mid = slow.next;
+  slow.next = null;
+  return merge(sortList(head), sortList(mid));
+};
+const merge = (list1, list2) => {
+  const dummy = new ListNode(0);
+  let cur = dummy;
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      cur.next = list1;
+      list1 = list1.next;
+    } else {
+      cur.next = list2;
+      list2 = list2.next;
+    }
+    cur = cur.next;
+  }
+  cur.next = list1 || list2;
+  return dummy.next;
+};
