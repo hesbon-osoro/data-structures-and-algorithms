8913c9b3bc73c4550142193321b5c9a5cc8a12c0
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-11-07 12:12:04 +0300

    one-liner

--- leetcode/maximum69Number.js
+++ leetcode/maximum69Number.js
@@ -2,10 +2,13 @@
  * @param {number} num
  * @return {number}
  */
-const maximum69Number = function (num) {
-  const str = num.toString();
-  const arr = str.split('');
-  const idx = arr.findIndex(n => n === '6');
-  if (idx !== -1) arr[idx] = '9';
-  return parseInt(arr.join(''), 10);
-};
+// const maximum69Number = function (num) {
+//   const str = num.toString();
+//   const arr = str.split('');
+//   const idx = arr.findIndex(n => n === '6');
+//   if (idx !== -1) arr[idx] = '9';
+//   return parseInt(arr.join(''), 10);
+// };
+
+// one liner
+const maximum69Number = num => parseInt(num.toString().replace('6', '9'));
