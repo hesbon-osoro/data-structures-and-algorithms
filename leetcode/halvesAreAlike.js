b4896a3f9eb65b492bfd4f197f35a12238da4497
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-12-01 09:00:28 +0300

    halvesAreAlike.js

---
+++ leetcode/halvesAreAlike.js
@@ -0,0 +1,27 @@
+/*
+You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+
+Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+
+Return true if a and b are alike. Otherwise, return false.
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const halvesAreAlike = function (s) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  let a = 0;
+  let b = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (vowels.includes(s[i])) {
+      if (i < s.length / 2) {
+        a++;
+      } else {
+        b++;
+      }
+    }
+  }
+  return a === b;
+};
