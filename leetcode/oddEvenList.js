fa354fc77a7b25840960f4da8c3bf8097947b330
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-12-06 16:44:30 +0300

    oddEvenList.js

---
+++ leetcode/oddEvenList.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const oddEvenList = function (head) {
+  if (!head) return null;
+  let odd = head;
+  let even = head.next;
+  let evenHead = even;
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+  odd.next = evenHead;
+  return head;
+};
