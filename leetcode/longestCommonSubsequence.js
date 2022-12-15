6bb85e46bdf8ab0da8ead1716b8b9ddd60866308
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-12-15 14:35:46 +0300

    longestCommonSubsequence.js

---
+++ leetcode/longestCommonSubsequence.js
@@ -0,0 +1,30 @@
+/*
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+*/
+
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+const longestCommonSubsequence = function (text1, text2) {
+  const dp = Array(text1.length + 1)
+    .fill(null)
+    .map(() => Array(text2.length + 1).fill(0));
+
+  for (let i = 1; i <= text1.length; i++) {
+    for (let j = 1; j <= text2.length; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  return dp[text1.length][text2.length];
+};
