b63f04c6b7fbd469793c06f66e59411c7cc7ecb4
Hesbon Osoro <hesbonosoro1@gmail.com>
2023-01-16 03:26:24 +0300

    insert.js

---
+++ leetcode/insert.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+const insert = function (intervals, newInterval) {
+  const result = [];
+  let i = 0;
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+    result.push(intervals[i]);
+    i++;
+  }
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+    i++;
+  }
+  result.push(newInterval);
+  while (i < intervals.length) {
+    result.push(intervals[i]);
+    i++;
+  }
+  return result;
+};
