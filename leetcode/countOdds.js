028c06edf605fca0cd511f491aa186a19928a4c0
Hesbon Osoro <hesbonosoro1@gmail.com>
2023-02-13 14:16:16 +0300

    countOdds.js

---
+++ leetcode/countOdds.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+const countOdds = function (low, high) {
+  let count = 0;
+  for (let i = low; i <= high; i++) {
+    if (i % 2 !== 0) {
+      count++;
+    }
+  }
+  return count;
+};
