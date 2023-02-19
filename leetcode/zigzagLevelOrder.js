36e760639e3f7ea9eb0c71844f7e972308fbf46a
Hesbon Osoro <hesbonosoro1@gmail.com>
2023-02-19 12:58:19 +0300

    zigzagLevelOrder.js

---
+++ leetcode/zigzagLevelOrder.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const zigzagLevelOrder = function (root) {
+  if (!root) return [];
+  const queue = [root];
+  const result = [];
+  let leftToRight = true;
+  while (queue.length) {
+    const level = [];
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      if (leftToRight) {
+        level.push(node.val);
+      } else {
+        level.unshift(node.val);
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(level);
+    leftToRight = !leftToRight;
+  }
+  return result;
+};
