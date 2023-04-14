ab900b32695712d53e5b67049dae59573feb2d70
Hesbon Osoro <hesbonosoro1@gmail.com>
2023-04-15 01:52:32 +0300

    longestPalindromeSubseq.js

---
+++ leetcode/longestPalindromeSubseq.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const longestPalindromeSubseq = function (s) {
+  const n = s.length;
+  const dp = Array.from({ length: n }, () => Array(n).fill(0));
+  for (let i = n - 1; i >= 0; i--) {
+    dp[i][i] = 1;
+    for (let j = i + 1; j < n; j++) {
+      if (s[i] === s[j]) {
+        dp[i][j] = dp[i + 1][j - 1] + 2;
+      } else {
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  return dp[0][n - 1];
+};
