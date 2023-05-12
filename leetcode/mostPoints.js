f01692730fe5b785df4cd06badc5fefee8a79296
Hesbon Osoro <hesbonosoro1@gmail.com>
2023-05-12 13:52:23 +0300

    mostPoints.js

---
+++ leetcode/mostPoints.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} questions
+ * @return {number}
+ */
+const mostPoints = questions => {
+  const n = questions.length;
+  const dp = new Array(n).fill(0);
+  let max = 0;
+
+  for (let i = n - 1; i >= 0; i--) {
+    const [point, brainpower] = questions[i];
+    dp[i] = Math.max(max, point + (dp[i + brainpower + 1] || 0));
+    max = Math.max(max, dp[i]);
+  }
+
+  return max;
+};
